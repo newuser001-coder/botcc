51a9eca6dc12dd28c1309379497255a19267bc83
user 100 <79753682+newuser001-coder@users.noreply.github.com>
2025-01-31 23:51:25 +0530

    Update config.js
    add token and bot user name

--- Killer Tool/config.js
+++ Killer Tool/config.js
@@ -2,7 +2,7 @@ module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
-    telegramBotToken: "7387279346:AAGdIyOX5qb61Lj_4upbbU8smUSdouMCR94", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["a"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["a"] // username or id of the user who can use the bot.
+    telegramBotToken: "8014293559:AAHNgEif_iJhNOJlh6nW6Ywr9NlYY6owrjg", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["@galaxy_carder"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["@galaxychecker_bot"] // username or id of the user who can use the bot.
 };
